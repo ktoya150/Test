3f453f425b9114d84aa48e5b1bbecfd114f856a3
ktoya150 <artemtkachenko07op@gmail.com>
2025-04-08 23:33:12 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "testfile.txt";
+        string content = "Тестовий файл з текстом";
+
+        // Запись текста в файл
+        File.WriteAllText(filePath, content);
+
+        Console.WriteLine("Файл успешно создан и записан.");
+    }
+}
